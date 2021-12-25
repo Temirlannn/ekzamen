79318e7f1a9d6d5a11b018771f9e6280bf369923
Temirlannn <93139917+Temirlannn@users.noreply.github.com>
2021-12-26 02:44:20 +0600

    add

--- app/src/main/java/com/itacademy/practically/models/Constants.kt
+++ app/src/main/java/com/itacademy/practically/models/Constants.kt
@@ -8,9 +8,9 @@ class Constants {
             News(Categories.Sport, "1","Спорт","https://api.живуспортом.рф/imagecache/full_jpeg/article_image/130/129249/5e5628e139feb.jpeg"),
             News(Categories.Sport, "2","Спорт","https://api.живуспортом.рф/imagecache/full_jpeg/article_image/130/129249/5e5628e139feb.jpeg"),
             News(Categories.Sport, "3","Спорт","https://api.живуспортом.рф/imagecache/full_jpeg/article_image/130/129249/5e5628e139feb.jpeg"),
-            News(Categories.weather, "4","Погода",""),
-            News(Categories.weather, "5","Погода",""),
-            News(Categories.weather, "6","Погода",""),
+            News(Categories.weather, "4","Погода","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLwPkCMn9hI9BERg4qTuVQs9F37aUbnvHCg&usqp=CAU"),
+            News(Categories.weather, "5","Погода","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLwPkCMn9hI9BERg4qTuVQs9F37aUbnvHCg&usqp=CAU"),
+            News(Categories.weather, "6","Погода","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLwPkCMn9hI9BERg4qTuVQs9F37aUbnvHCg&usqp=CAU"),
         )
         val categories = arrayListOf(
             CategoryModel(Categories.Sport,R.string.Sports,getNewsByCategories(Categories.Sport)),
